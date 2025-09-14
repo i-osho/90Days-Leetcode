12851f831a4b9d8a60df9f097d264b0363f51a6c
Osho (VSCode) <i-osho@users.noreply.github.com>
2025-09-14 18:23:55 +0530

    Day 23 (delayed)

---
+++ Day 23/Remove Nth Node From End (LC 19).py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        p = dummy
+        q = dummy
+        
+        for _ in range(n):
+            q = q.next
+            
+        while q.next:
+            p = p.next
+            q = q.next
+            
+        p.next = p.next.next
+        
+        return dummy.next
